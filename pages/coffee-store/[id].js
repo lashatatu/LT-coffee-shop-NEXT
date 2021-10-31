217137b1dd7610e60035f88a39da6180614f7e8a
lasha tatulashvili <lashatatu@gmail.com>
2021-10-31 16:22:06 +0400

    27

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,14 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const CoffeeStore = () => {
-  const router=useRouter()
+  const router = useRouter();
   return (
     <div>
+      {router.query.id}{" "}
+      <Link href="/">
+        <a>Back To Home</a>
+      </Link>
       <p>CoffeeStore</p>
     </div>
   );
