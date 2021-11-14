9b7466981c06feb46b0551d69ebb5f65937ca157
lasha tatulashvili <lashatatu@gmail.com>
2021-11-14 04:36:47 +0400

    60 getStaticPaths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,5 +1,25 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (coffeeStore) => coffeeStore.id === params.id
+      ),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: coffeeStoresData.map((coffeeStore) => ({
+      params: { id: coffeeStore.id },
+    })),
+    fallback: false,
+  };
+}
 
 const CoffeeStore = () => {
   const router = useRouter();
