a98ac6f2e4f406e7d1dd23c249f5b422257316c5
lasha tatulashvili <lashatatu@gmail.com>
2021-11-22 05:29:12 +0400

    73 lib

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,17 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "../Components/Banner";
 import Card from "../Components/Card";
-// import coffeeStoresData from "../data/coffee-stores.json";
+import { fetchCoffeeStores } from "../lib/coffee-stores";
 
-export async function getStaticProps (context) {
-  const response = await fetch(
-    `https://api.foursquare.com/v2/venues/search?ll=41.705922751666556, 44.78714478305549&query=coffee stores&client_id=${process.env.FOURSQUARE_CLIENT_ID}&client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}&v=20210525&limit=6`
-  );
-  const data =await response.json();
-  console.log(data);
+export async function getStaticProps(context) {
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
-    props: { coffeeStores: data.response.venues },
+    props: { coffeeStores },
   };
 }
 
-export default function Home (props) {
+export default function Home(props) {
   const handleOnBannerBtnClick = () => {
     console.log("clicked on button");
   };
@@ -49,7 +46,10 @@ export default function Home (props) {
                 <Card
                   key={coffeeStore.id}
                   name={coffeeStore.name}
-                  imgUrl={coffeeStore.imgUrl || `https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80`}
+                  imgUrl={
+                    coffeeStore.imgUrl ||
+                    `https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80`
+                  }
                   href={`/coffee-store/${coffeeStore.id}`}
                   className={styles.card}
                 />
